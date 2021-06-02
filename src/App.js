06b6c6c818adc0d42e89c1495f0d6a17594f6cac
fattah <fattahmuhyiddeen@gmail.com>
2021-06-02 23:45:54 +0800

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
 
+const PER_PAGE = 10;
+
 function App() {
+  const [result, setResult] = useState([]);
+  const [page, setPage] = useState(1);
+  useEffect(()=>{
+    search();
+  },[])
+  const search = async() => {
+    // const response = await fetch('https://api.github.com/search/repositories?q=language:js', {
+    const response = await fetch(`https://api.github.com/search/topics?q=js&per_page=${PER_PAGE}&page=${page}`, {
+      headers: {
+        Accept: 'application/vnd.github.mercy-preview+json',
+      },
+    });
+    const data = await response.json();
+    setResult(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {result?.items?.map(r => (
+        <pre>{JSON.stringify(r, undefined, 2)}</pre>
+      ))}
     </div>
   );
 }
