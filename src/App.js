4ede0d9b72995127bde0b4a5ae8cd680caea799d
fattah <fattahmuhyiddeen@gmail.com>
2021-06-03 23:23:40 +0800

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,68 +1,84 @@
 import { useEffect, useState } from 'react';
-import { TablePagination, Select, MenuItem, TextField } from '@material-ui/core';
+import {
+  TablePagination, Select, MenuItem, TextField, FormControl, InputLabel, Button, CircularProgress,
+} from '@material-ui/core';
+import { Search } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  input: {
+    marginRight: '2em',
+  },
+}));
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const classes = useStyles();
   const [result, setResult] = useState({});
   const [language, setLanguage] = useState(0);
   const [topic, setTopic] = useState('');
   const [page, setPage] = useState(1);
   const [perPage, setPerPage] = useState(10);
-  useEffect(()=>{
-    search();
-  },[page,perPage])
 
-  const search = async() => {
-    const isTopic = !!topic;
-    // const response = await fetch('https://api.github.com/search/repositories?q=topic+language:js', {
-    // const response = await fetch('https://api.github.com/search/repositories?q=language:js', {
-    const response = await fetch(`https://api.github.com/search/topics?q=microsoft&per_page=${perPage}&page=${page}`, {
-      headers: {
-        Accept: 'application/vnd.github.mercy-preview+json',
-      },
-    });
+  const search = async () => {
+    if (!topic && !language) return;
+    setIsLoading(true);
+    let url = 'https://api.github.com/search/repositories?q=';
+    if (topic) url += topic;
+    if (topic && language) url += '+';
+    if (language) url += `language:${language}`;
+    const response = await fetch(`${url}&per_page=${perPage}&page=${page}`);
     setResult(await response.json());
-  }
+    setIsLoading(false);
+  };
 
-  const handleChangeLanguage = (e) => {
-    setLanguage(e.target.value);
-    setTopic('');
-  };
-  const handleChangeTopic = (e) => {
-    setTopic(e.target.value);
-    setLanguage(0);
-  };
+  useEffect(() => {
+    search();
+  }, [page, perPage]);
+
+  const handleChangeLanguage = (e) => setLanguage(e.target.value);
+  const handleChangeTopic = (e) => setTopic(e.target.value);
+
   return (
     <div>
-      <Select
+      <FormControl className={classes.input}>
+        <InputLabel shrink>Language</InputLabel>
+        <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           value={language}
           onChange={handleChangeLanguage}
         >
           <MenuItem value={0}>Search Language</MenuItem>
-          <MenuItem value={'HTML'}>HTML</MenuItem>
-          <MenuItem value={'java'}>Java</MenuItem>
-          <MenuItem value={'Ruby'}>Ruby</MenuItem>
-          <MenuItem value={'kotlin'}>Kotlin</MenuItem>
-          <MenuItem value={'swift'}>Swift</MenuItem>
-          <MenuItem value={'js'}>JS</MenuItem>
+          <MenuItem value="HTML">HTML</MenuItem>
+          <MenuItem value="java">Java</MenuItem>
+          <MenuItem value="Ruby">Ruby</MenuItem>
+          <MenuItem value="kotlin">Kotlin</MenuItem>
+          <MenuItem value="swift">Swift</MenuItem>
+          <MenuItem value="js">JS</MenuItem>
         </Select>
-         <TextField label="Search Topic" onChange={handleChangeTopic} value={topic} />
+      </FormControl>
+      <TextField className={classes.input} size="small" label="Search Topic" onChange={handleChangeTopic} value={topic} />
+      <Button
+        variant="contained"
+        color="secondary"
+        startIcon={<Search />}
+        onClick={search}
+        disabled={!language && !topic}
+      >
+        Search
+      </Button>
       <TablePagination
         page={page}
-        onChangeRowsPerPage={e=>setPerPage(parseInt(e.target.value, 10))}
+        onChangeRowsPerPage={(e) => setPerPage(parseInt(e.target.value, 10))}
         rowsPerPage={perPage}
-        labelDisplayedRows={({ from, to, count }) =>`${from}-${to} of ${count}`}
-        onChangePage={(_,v)=>setPage(v)}
-        count={Math.ceil(((result?.total_count)||0) / perPage)}
+        labelDisplayedRows={({ from, to, count }) => `${from}-${to} of ${count}`}
+        onChangePage={(_, v) => setPage(v)}
+        count={Math.ceil(((result?.total_count) || 0) / perPage)}
       />
+      {isLoading && <div style={{margin: '1em'}}><CircularProgress /></div>}
       <div className="result">
-        {result?.items?.map((r, index) => (
-          <>
-            <div>{index + 1 + (page * perPage)}</div>
-            <pre>{JSON.stringify(r, undefined, 2)}</pre>
-          </>
-        ))}
+        {result?.items?.map((r) => <pre>{JSON.stringify(r, undefined, 2)}</pre>)}
       </div>
     </div>
   );
